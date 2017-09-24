2da68ac7353bb1e06880833b5315f165ed31c73d
stprost <stprostakov@gmail.com>
2017-09-24 22:00:09 +0400

    24.09

--- src/MaxSubarray.java
+++ src/MaxSubarray.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -7,15 +8,7 @@ public class MaxSubarray {
     public static void main(String[] args) {
         fill(a);
         System.out.println(Arrays.toString(a));
-        System.out.println();
-        fill(a);
-        System.out.println(Arrays.toString(a));
-        System.out.println();
-        fill(a);
-        System.out.println(Arrays.toString(a));
-        System.out.println();
-        fill(a);
-        System.out.println(Arrays.toString(a));
+        System.out.println(max(a).toString());
     }
 
     public static void fill(int a[]) {
@@ -26,7 +19,42 @@ public class MaxSubarray {
         }
     }
 
-   // public static int[] max(int a[]) {
+    public static int elemSum(ArrayList<Integer> a) {
+        int sum = 0;
+        for (Integer el : a) {
+            sum += el;
+        }
+        return sum;
+    }
 
-   // }
+    public static ArrayList max(int a[]) {
+        ArrayList ans = new ArrayList();
+        ArrayList temp = new ArrayList();
+        ans.add(a[0]);
+        temp.add(a[0]);
+        int n = a.length;
+        if (n == 1) return ans;
+        else for (int i = 1; i < n; i++) {
+            if (elemSum(temp) > elemSum(ans)) {
+                ans.clear();
+                temp.forEach(ans::add);
+            }
+            if (elemSum(temp) <= 0) {
+                if (elemSum(temp) <= a[i]) {
+                    temp.clear();
+                    temp.add(a[i]);
+                }
+            } else if (elemSum(temp) + a[i] <= 0) {
+                temp.clear();
+                temp.add(a[i]);
+            } else temp.add(a[i]);
+        }
+
+        if (elemSum(temp) > elemSum(ans)) {
+            ans.clear();
+            temp.forEach(ans::add);
+        }
+        return ans;
+    }
 }
+
