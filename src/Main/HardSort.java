480e5e7c696a7489379eec1a1f160351640d0fe2
SidorenkoNikita <Teryn=136>
2017-09-28 15:33:38 +0300

    28.09

--- src/Main/HardSort.java
+++ src/Main/HardSort.java
@@ -1,8 +1,10 @@
 package Main;
 
-public class HardSort {
+import java.util.Arrays;
 
-    public static void f2(int a[], int k, int n) {
+public class HardSort {
+    //HeapSort
+    public static void f(int a[], int k, int n) {
         int t = a[k];
         int max;
         if (k * 2 + 2 > n) max = k * 2 + 1;
@@ -12,17 +14,54 @@ public class HardSort {
             a[k] = a[max];
             a[max] = t;
         }
-        if (max * 2 + 1 <= n) f2(a, max, n);
+        if (max * 2 + 1 <= n) f(a, max, n);
     }
 
-    public static void heapsort(int a[]) {
+    public static void heapSort(int a[]) {
         int n = a.length;
-        for (int i = n / 2 - 2; i >= 0; i--) f2(a, i, n);
+        for (int i = n / 2 - 2; i >= 0; i--) f(a, i, n);
         for (int i = n - 1; i > 0; i--) {
-            f2(a, 0, i);
+            f(a, 0, i);
             int t = a[0];
             a[0] = a[i];
             a[i] = t;
+        }
+    }
+
+    //QuickSort
+    public static void f1(int a[], int l, int m) {
+        int t = a[(m + l) / 2];
+        int k;
+        int i = l;
+        int j = m;
+        while (i <= j) {
+            if (a[i] >= t && a[j] <= t) {
+                k = a[i];
+                a[i] = a[j];
+                a[j] = k;
+                i++;
+                j--;
+            } else {
+                if (a[i] < t) i++;
+                if (a[j] > t) j--;
+            }
+        }
+        if (j > l) f1(a, l, j);
+        if (i < m) f1(a, i, m);
+    }
+
+    public static void quickSort(int a[]) {
+        f1(a, 0, a.length - 1);
+    }
+
+    //Merge sort
+    public static void merge(int a[], int l, int m, int r) {
+        int left[] = Arrays.copyOfRange(a, l, m + 1);
+        int right[] = Arrays.copyOfRange(a, m + 1, r + 1);
+        int i = 0;
+        int j = 0;
+        for (int k = 0; k <= r; k++){
+            //if (left[i] < right[j]) a[i]
         }
     }
 }
--- src/Main/Mas.java
+++ src/Main/Mas.java
@@ -1,11 +1,11 @@
 package Main;
 
 public class Mas {
-    final static int a[] = {9, 2, 7, 4, 3, 8, 1, 5, 6, 0};
+    final static int a[] = {8, 2, 1, 3};
 
     public static void main(String[] args) {
         Sorted.prn(a);
-        HardSort.heapsort(a);
+        HardSort.heapSort(a);
         System.out.println("Sorted:");
         Sorted.prn(a);
     }
